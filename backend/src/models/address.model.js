aef672de4cd9ee92663e2c1b23291120ad1e95d4
Rajat Mangla <rajatmangla0203@gmail.com>
2025-01-28 23:09:39 +0530

    added add address function

---
+++ backend/src/models/address.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    street: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    postalCode: {
+        type: String,
+        required: true,
+    },
+    isDefault: {
+        type: Boolean,
+        default: false,
+    },
+},{timestamps : true});
+
+export const Address = mongoose.model("Address",addressSchema);
