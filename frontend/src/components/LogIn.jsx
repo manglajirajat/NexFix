11e3560dc7d991c9d6deb74740af6fd9b263fcb1
Prince Jha <156680708+PrinceJha08@users.noreply.github.com>
2025-02-11 12:27:28 +0530

    sign in and logout

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Typography, Input, Button } from "@material-tailwind/react";
+import { EyeSlashIcon, EyeIcon } from "@heroicons/react/24/solid";
 
 export default function LogIn() {
     const [data, setData] = useState({ email: "", password: "" });
@@ -7,11 +9,14 @@ export default function LogIn() {
     const [loading, setLoading] = useState(true);
     const [loginLoading, setLoginLoading] = useState(false);
     const [error, setError] = useState("");
+    const [passwordShown, setPasswordShown] = useState(false);
     const navigate = useNavigate();
+
+    const togglePasswordVisibility = () => setPasswordShown((cur) => !cur);
 
     const handleChange = (e) => {
         setData({ ...data, [e.target.name]: e.target.value });
-        setError(""); // Clear error when user types
+        setError("");
     };
 
     const handleSubmit = async (e) => {
@@ -39,7 +44,7 @@ export default function LogIn() {
 
             const result = await response.json();
             setProfile(result.data.user);
-            localStorage.setItem("accessToken", result.data.accessToken); 
+            localStorage.setItem("accessToken", result.data.accessToken);
             navigate("/");
             window.location.reload();
         } catch (error) {
@@ -74,104 +79,94 @@ export default function LogIn() {
         }
     };
 
-    const handleLogout = () => {
-        localStorage.removeItem("accessToken");
-        setProfile(null);
-        navigate("/");
-        window.location.reload();
+    const logOut = () => {
+        localStorage.removeItem("accessToken"); // Clear the access token
+        setProfile(null); // Reset the profile state
+        navigate("/"); // Redirect to the login page
     };
 
     useEffect(() => {
-        const check = async () =>{
-            await checkAuthStatus();
-        }
-
-        check();
+        checkAuthStatus();
     }, []);
 
     return (
-        <div className="p-4">
-            {loading ? (
-                <p>Loading...</p>
-            ) : !profile ? (
+        <section className="grid text-center h-screen items-center p-8">
+            {!profile ? (
                 <div>
-                    <h2 className="m-2">Log in first</h2>
-                    {error && <p className="text-red-500">{error}</p>}
-                    <form onSubmit={handleSubmit}>
-                        <div className="m-2">
-                            <label htmlFor="email">Enter email:</label>
-                            <input
+                    <Typography variant="h3" color="blue-gray" className="mb-2">
+                        Sign In
+                    </Typography>
+                    <Typography className="mb-16 text-gray-600 font-normal text-[18px]">
+                        Enter your email and password to sign in
+                    </Typography>
+                    {error && <Typography color="red" className="mb-4">{error}</Typography>}
+                    <form onSubmit={handleSubmit} className="mx-auto max-w-[24rem] text-left">
+                        <div className="mb-6">
+                            <label htmlFor="email">
+                                <Typography variant="small" className="mb-2 block font-medium text-gray-900">
+                                    Your Email
+                                </Typography>
+                            </label>
+                            <Input
+                                id="email"
+                                color="gray"
+                                size="lg"
                                 type="email"
-                                className="px-2 rounded-full border-2 border-slate-200"
-                                value={data.email}
                                 name="email"
+                                placeholder="name@mail.com"
+                                value={data.email}
                                 onChange={handleChange}
+                                className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
                             />
                         </div>
-
-                        <div className="m-2">
-                            <label htmlFor="password">Enter password:</label>
-                            <input
-                                type="password"
-                                className="px-2 rounded-full border-2 border-slate-200"
-                                value={data.password}
+                        <div className="mb-6">
+                            <label htmlFor="password">
+                                <Typography variant="small" className="mb-2 block font-medium text-gray-900">
+                                    Password
+                                </Typography>
+                            </label>
+                            <Input
+                                size="lg"
+                                placeholder="********"
+                                className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
+                                type={passwordShown ? "text" : "password"}
                                 name="password"
+                                value={data.password}
                                 onChange={handleChange}
+                                icon={
+                                    <i onClick={togglePasswordVisibility}>
+                                        {passwordShown ? <EyeIcon className="h-5 w-5" /> : <EyeSlashIcon className="h-5 w-5" />}
+                                    </i>
+                                }
                             />
                         </div>
-
-                        <button
-                            className="rounded-full p-2 px-4 m-2 bg-blue-500 text-white disabled:opacity-50"
-                            type="submit"
-                            disabled={loginLoading}
-                        >
-                            {loginLoading ? "Logging in..." : "Log in now"}
-                        </button>
+                        <Button type="submit" color="gray" size="lg" className="mt-6" fullWidth disabled={loginLoading}>
+                            {loginLoading ? "Logging in..." : "Sign In"}
+                        </Button>
                     </form>
                 </div>
             ) : (
                 <div>
-                    <h2>My Profile</h2>
-                    <p>ID: {profile._id}</p>
-                    <p>Name: {profile.name}</p>
-                    <p>conact : {profile.contactNumber}</p>
-                    <p>email : {profile.email}</p>
-                    <div>
-                        {(profile.address != 0) ? (
-                            <div>
-                                <p>address : </p>
-                                {profile.address.map((address) => (
-                                    <p key={address._id}>
-                                        {address.street}, {address.city}, {address.state}, {address.postalCode} {(address.isDefault) ? "Default" : ""}
-                                    </p>
-                                ))}
-                            </div>
-                        ) : (
-                            <p>no address</p>
-                        ) }
-                    </div>
-                    <div>
-                        <p>cart id : {profile.cart._id}</p>
-                        {profile.cart.items.length != 0 ? (
-                            profile.cart.items.map((item) => (
-                                <div key={item._id}>
-                                    <p>product id : {item.product}</p>
-                                    <p>quantity : {item.quantity}</p>
-                                </div>
-                            ))
-                        ) : (
-                            <p>no items in cart</p>
-                        )}
-                    </div>
-                    <p>cart price : {profile.cart.total}</p>
-                    <button
-                        onClick={handleLogout}
-                        className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
+                    <Typography variant="h4" color="blue-gray" className="mb-4">
+                        Profile Details
+                    </Typography>
+                    <Typography className="mb-2">
+                        <strong>Name:</strong> {profile.name}
+                    </Typography>
+                    <Typography className="mb-4">
+                        <strong>Email:</strong> {profile.email}
+                    </Typography>
+                    <Button
+                        color="red"
+                        size="lg"
+                        className="mt-4"
+                        fullWidth
+                        onClick={logOut} // Call the logOut function on button click
                     >
-                        Logout
-                    </button>
+                        Log Out
+                    </Button>
                 </div>
             )}
-        </div>
+        </section>
     );
 }
